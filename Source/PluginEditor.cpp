8d56a629ba65719fceb83bd02ca9c6b0fc835ca8
“Vincent <louise.insomnia@gmail.com>
2021-04-20 20:16:47 +0200

    initial commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,55 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+NewProjectAudioProcessorEditor::NewProjectAudioProcessorEditor (NewProjectAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (200, 400);
+    
+    gainSlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+    gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 80, 25);
+    gainSlider.setRange(0.0, 1.0);
+    gainSlider.setValue(0.5);
+    gainSlider.addListener(this);
+    addAndMakeVisible(gainSlider);
+}
+
+NewProjectAudioProcessorEditor::~NewProjectAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void NewProjectAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    
+    
+}
+
+void NewProjectAudioProcessorEditor::resized()
+{
+    gainSlider.setBounds(getLocalBounds());
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
+
+void NewProjectAudioProcessorEditor::sliderValueChanged(juce::Slider *slider){
+    
+    if (slider == &gainSlider){
+        audioProcessor.rawVolume = gainSlider.getValue();
+    }
+}
+
